25ec9074211c940bdf346388978375278b9e52f4
Kim Sang Ju <cheney3193@naver.com>
2025-05-20 21:40:21 +0900

    불필요한 코드 정리
    TargetCenter 오브젝트의 위치를 기준으로 하지않고, Target 오브젝트의 BoxCollider의 Center값을 참조하도록 코드 변경

--- Assets/BamsongiController.cs
+++ Assets/BamsongiController.cs
@@ -5,35 +5,21 @@ using UnityEngine;
 
 public class BamsongiController : MonoBehaviour
 {
-    Transform targetCenter = null;  //과녁의 중심점 투명 오브젝트의 좌표값 
+    GameObject gTarget = null; //과녁 오브젝트 오브젝트 변수
     BoxCollider boxCollider = null; //과녁의 Box Collider 값을 가져오기 위함
 
-    Vector3 vHitPoint = Vector3.zero;   //밤송이의 충돌위치 좌표값 벡터
-    Vector2 vHitXY = Vector2.zero;      //거리 계산을 위한 밤송이의 X, Y좌표값 벡터
+    Vector2 vHitXY = Vector2.zero;      //거리 계산을 위한 밤송이의 타격점 X, Y좌표값 벡터
     Vector2 vCenterXY = Vector2.zero;   //거리 계산을 위한 중심점 X, Y좌표값 벡터
 
     float fDistance = 0.0f;     //밤송이 타격지점과, 원의 중심까지의 거리
     float fMaxRadius = 0.0f;    //과녁의 크기
-
-    float fKillObjTime = 3.0f; //오브젝트 삭제 시간
+    float fKillObjTime = 3.0f;  //오브젝트 삭제 시간
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //TargetCenter 위치를 기준으로 점수 계산하므로 TargetCenter의 transform 값을 불러온다
-        targetCenter = GameObject.Find("TargetCenter").transform;
-
-        //TargetCenter의 부모(Target)에서 BoxCollider를 가져온다.
-        //Target 오브젝트를 Find하여도 되지만 Target과 TargetCenter는 상속관계이므로 아래와 같이 작성함
-        boxCollider = targetCenter.transform.parent.GetComponent<BoxCollider>();
-
-        /*
-         * 밤송이가 화면 안쪽으로 날아가도록 +Z축 방향의 벡터를 매개변수로 전달하고 f_TargetShoot 메소드 호출
-         * Y축 방향으로 힘을 200.0f 가하는 이유는 밤송이가 과녁에 닫기 전에 중력의 영향을 받아
-         * 지면으로 낙하하는 것을 막기 위함
-         * Start 메소드를 호출하는 시작과 동시에 밤송이가 과녁으로 날아감
-         */
-        //f_TargetShoot(new Vector3(0.0f, 200.0f, 2000.0f));
+        gTarget = GameObject.Find("Target");                //과녁 오브젝트 불러오기
+        boxCollider = gTarget.GetComponent<BoxCollider>();  //과녁 오브젝트의 BoxCollider 기능 불러오기
     }
 
     // Update is called once per frame
@@ -42,10 +28,14 @@ public class BamsongiController : MonoBehaviour
         
     }
 
-    /// <summary>
-    /// 매개변수 방향으로 밤송이에게 힘을 가하는 메소드
-    /// </summary>
-    /// <param name="argDir">방향</param>
+    /*
+     * 밤송이가 화면 안쪽으로 날아가도록 +Z축 방향의 벡터를 매개변수로 전달하고 f_TargetShoot 메소드 호출
+     * Y축 방향으로 힘을 200.0f 가하는 이유는 밤송이가 과녁에 닫기 전에 중력의 영향을 받아
+     * 지면으로 낙하하는 것을 막기 위함
+     * Start 메소드를 호출하는 시작과 동시에 밤송이가 과녁으로 날아감
+     */
+
+    ///<summary>밤송이에게 힘을 가하는 메소드</summary>
     public void f_TargetShoot(Vector3 argDir)
     {
         //매개변수로 전달된 Vector값으로 힘을 가한다.
@@ -63,16 +53,9 @@ public class BamsongiController : MonoBehaviour
         GetComponent<Rigidbody>().isKinematic = true;
         GetComponent<ParticleSystem>().Play(); //파티클 재생
 
-        
-        if (targetCenter == null)
+        if (boxCollider == null) //BoxCollider가 없는 경우
         {
-            Debug.LogWarning("TargetCenter를 찾을 수 없습니다.");
-            return;
-        }
-        
-        if (boxCollider == null)
-        {
-            Debug.LogWarning("Target에 BoxCollider가 없습니다.");
+            Debug.LogWarning($"{gTarget.gameObject.name}에 BoxCollider가 없습니다.");
             return;
         }
 
@@ -82,19 +65,13 @@ public class BamsongiController : MonoBehaviour
         /*
          * 밤송이가 Sphere Collider 형태라 타격지점이 모호할 수 있는 문제가 발생함
          * collision.contacts[0].point를 사용하여 정확한 충돌 접점 위치값을 사용함
+         * 그러나, collision.contacts[0].point 값을 그대로 사용하면 z축값이 계산에 포함됨
+         * z축값은 원의 중심에 타격해도 거리는 크게 계산되는 문제가 발생함
+         * 따라서 Vector2를 사용하여 x,y값만 반영함
          */
-        vHitPoint = collision.contacts[0].point;
-
-        /*
-         * collision.contacts[0].point 값을 그대로 사용하면
-         * TargetCenter가 Target의 내부에 존재하여 z값이 계산에 포함됨
-         * 즉, Vector3.Distance 사용시 z값이 포함되기 때문에 실제 원의 중심에 타격해도 거리는 크게 계산되는 문제가 발생함
-         * 이 문제를 해결하기위해, z축 값은 제외시키로 결정함
-         */
-        vHitXY = new Vector2(vHitPoint.x, vHitPoint.y);
-        vCenterXY = new Vector2(targetCenter.position.x, targetCenter.position.y);
-
-        fDistance = Vector2.Distance(vHitXY, vCenterXY);
+        vHitXY = collision.contacts[0].point; //타격지점 저장
+        vCenterXY = new Vector2(boxCollider.center.x, boxCollider.center.y); //중심점 저장
+        fDistance = Vector2.Distance(vHitXY, vCenterXY); //타격지점과 중심점간의 거리 계산
 
         GameManager.Instance.f_AddScoreByDistance(fDistance, fMaxRadius); //점수 계산 및 누적 처리
 
